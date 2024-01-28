b47a8b9bb2676a55f5144442e9046c2bec74cce9
Nonegop <nice-1791@mail.ru>
2024-01-28 11:10:37 +0400

    Add Добавлен модификатор доступа к методу update

--- Assets/Scripts/CubeTurner.cs
+++ Assets/Scripts/CubeTurner.cs
@@ -4,7 +4,7 @@ public class CubeTurner : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    void Update()
+    private void Update()
     {
         transform.Rotate(Vector3.up, _speed * Time.deltaTime);
     }
